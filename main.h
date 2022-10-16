9b7433d8b36b3bb79b970a5301e7aabd252eac65
Iyanu456 <oyerindei13@gmail.com>
2022-10-16 15:36:03 -0700

    new

--- main.h
+++ main.h
@@ -1,3 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+typedef struct
+{
+	char *percent;
+	char *type;
+	char *symbol;
+	char (*f)(char *str);
+} printer_func ;
+
+int _printf(const char *format, ...);
 #endif
