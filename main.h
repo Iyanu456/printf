24bb6328ecd0fab9c03b622f2b4f5d3c9ad0641a
Iyanu456 <oyerindei13@gmail.com>
2022-10-18 20:37:06 -0700

    default message

--- main.h
+++ main.h
@@ -18,20 +18,23 @@ struct fmt_op
 {
 	char fmt;
 	int (*func)(va_list, char[], int, int, int, int);
-} fmt_t;
+};
+typedef struct fmt_op fmt_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _isdigit(int c);
 
-int (*get_printf_func)(va_list, char[], int, int, int, int);
+int handle_print(const char *fmt, int *id, va_list list, char buffer[], int flags, int width, int precision, int size);
 
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 int print_int(va_list types, char buffer[], int flags, int width, int presision, int size);
-
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
 int get_flags(const char *format, int *i);
 int get_precision(const char *format, int *i, va_list list);
 int get_width(const char* format, int *i, va_list list);
