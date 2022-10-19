aeab36b4964be164b381d3d809ae9e51ddc9bb82
Iyanu456 <oyerindei13@gmail.com>
2022-10-19 06:16:41 -0700

    default message

--- main.h
+++ main.h
@@ -17,12 +17,12 @@
 #define S_LONG 2
 #define S_SHORT 1
 
-struct fmt
+typedef struct fmt
 {
 	char fmt;
 	int (*func)(va_list, char[], int, int, int, int);
-};
-typedef struct fmt_op fmt_t;
+} fmt_t;
+
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -30,6 +30,7 @@ int _isdigit(int c);
 int is_digit(int c);
 
 int handle_print(const char *fmt, int *id, va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
 
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
