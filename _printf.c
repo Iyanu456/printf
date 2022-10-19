69806fb86ef95b34453889eefed27d5f6636500a
Iyanu456 <110264592+Iyanu456@users.noreply.github.com>
2022-10-19 04:50:09 +0100

    Update _printf.c
    Changed BUFF_SIZE to BUFFER_SIZE

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char buffer[BUFFER_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
+			if (buff_ind == BUFFER_SIZE)
 				print_buffer(buffer, &buff_ind);
 			printed_chars++;
 		}
